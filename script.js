417bd9501e5f4547d60b957c5c3928e52b67cd67
A-Warsame <aawarsame12@gmail.com>
2024-07-08 18:12:14 +0100

    Added playGame function
    - Ensure new human and computer choices are generated each round
    - Update and log scores after each round

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-const moves = ['rock','paper','scissors'];
+const moves = ['rock', 'paper', 'scissors'];
 const lowerCaseMoves = moves.map(move => move.toLowerCase());
 let humanScore = 0;
 let computerScore = 0;
 
 function getComputerChoice() {
     let randomMove = lowerCaseMoves[Math.floor(lowerCaseMoves.length * Math.random())];
-    return randomMove
+    return randomMove;
 }
 
 function getHumanChoice() {
-    let humanMove = prompt("Choose your move (rock, paper, scissors): ");
+    let humanMove = prompt("Choose your move (rock, paper, scissors): ").toLowerCase();
     
-    if (moves.includes(humanMove)) {
+    if (lowerCaseMoves.includes(humanMove)) {
         return humanMove;
     } else {
         console.log("Use a valid move (rock, paper, scissors)");
@@ -27,17 +27,26 @@ function playRound(humanChoice, computerChoice) {
         (humanChoice === "paper" && computerChoice === "rock") ||
         (humanChoice === "scissors" && computerChoice === "paper")
     ) {
-        console.log(`You win. Computer chose ${computerChoice}`);
+        // console.log(`You win. Computer chose ${computerChoice}`);
         humanScore++;
     } else {
-        console.log(`You lose... Computer chose ${computerChoice}`);
+        // console.log(`You lose... Computer chose ${computerChoice}`);
         computerScore++;
     }
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+function playGame() {    
+    let round = 1;
 
-playRound(humanSelection, computerSelection);
+    while (round <= 5) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+        console.log(`Round ${round} - Human ${humanScore} : ${computerScore} Computer`);
+        round++;
+    }
 
-console.log(`You ${humanScore} : ${computerScore} Computer`)
+    console.log(`Final score \nHuman ${humanScore} : ${computerScore} Computer`);
+}
+
+playGame();
