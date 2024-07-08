dcc19d01fe0d9d3c2ae5178257711f0b39ede411
A-Warsame <aawarsame12@gmail.com>
2024-07-08 13:29:13 +0100

    Added getComputerMove and getHumanChoice functions
    - Defined valid moves ['r', 'p', 's']
    - Implemented getComputerMove to select a random move
    - Added getHumanChoice to prompt user and validate input
    - Ensured recursive input prompt for invalid choices

---
+++ script.js
@@ -0,0 +1,18 @@
+const moves = ['r','p','s']
+const lowerCaseMoves = moves.map(move => move.toLowerCase());
+
+function getComputerMove() {
+    let randomMove = lowerCaseMoves[Math.floor(lowerCaseMoves.length * Math.random())];
+    return randomMove
+}
+
+function getHumanChoice() {
+    let humanMove = prompt("Choose your move (r, p, s): ");
+    
+    if (moves.includes(humanMove)) {
+        return humanMove;
+    } else {
+        console.log("Use a valid move (r, p, s)");
+        return getHumanChoice(); 
+    }
+}
