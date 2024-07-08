057cf19c7e105d1ba6bb2160c1152bfa2a5bcec1
A-Warsame <aawarsame12@gmail.com>
2024-07-08 14:40:25 +0100

    Add playRound function and game logic: - Define function playRound to determine game outcome based on choices - Implement logic for win, lose, and draw conditions - Increment humanScore and computerScore based on result - Add calls to getHumanChoice and getComputerChoice functions - Display game result and scores in console

--- script.js
+++ script.js
@@ -1,20 +1,43 @@
-const moves = ['r','p','s'];
+const moves = ['rock','paper','scissors'];
 const lowerCaseMoves = moves.map(move => move.toLowerCase());
 let humanScore = 0;
 let computerScore = 0;
 
-function getComputerMove() {
+function getComputerChoice() {
     let randomMove = lowerCaseMoves[Math.floor(lowerCaseMoves.length * Math.random())];
     return randomMove
 }
 
 function getHumanChoice() {
-    let humanMove = prompt("Choose your move (r, p, s): ");
+    let humanMove = prompt("Choose your move (rock, paper, scissors): ");
     
     if (moves.includes(humanMove)) {
         return humanMove;
     } else {
-        console.log("Use a valid move (r, p, s)");
+        console.log("Use a valid move (rock, paper, scissors)");
         return getHumanChoice(); 
     }
 }
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log(`Draw. Computer chose ${computerChoice}`);
+    } else if (
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+        console.log(`You win. Computer chose ${computerChoice}`);
+        humanScore++;
+    } else {
+        console.log(`You lose... Computer chose ${computerChoice}`);
+        computerScore++;
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
+console.log(`You ${humanScore} : ${computerScore} Computer`)
